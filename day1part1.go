e7d77a964b25075a88fa061b3519f643d4210445
Luna Razzaghipour <lunarazzaghipour@gmail.com>
2023-12-04 10:42:37 +1100

    Day 1 Part 1

---
+++ day1part1.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	file, err := os.ReadFile("day1input")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: failed to read input\n")
+		return
+	}
+
+	trimmed := strings.Trim(string(file), "\n")
+	lines := strings.Split(trimmed, "\n")
+	sum := 0
+
+	for _, line := range lines {
+		var first, last int
+
+		for _, b := range line {
+			isDigit := b >= '0' && b <= '9'
+			if !isDigit {
+				continue
+			}
+
+			digit := int(b - '0')
+			if first == 0 {
+				first = digit
+			}
+			last = digit
+		}
+
+		sum += first*10 + last
+	}
+
+	fmt.Println(sum)
+}

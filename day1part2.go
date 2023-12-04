c29605bb1c22238ac1ffde6eb21dbee062ed234b
Luna Razzaghipour <lunarazzaghipour@gmail.com>
2023-12-04 12:19:07 +1100

    Day 1 Part 2

---
+++ day1part2.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"os"
+	"strings"
+)
+
+func main() {
+	file, err := os.ReadFile("day1input")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: failed to read input\n")
+		return
+	}
+
+	digits := []string{
+		"one", "two", "three", "four", "five",
+		"six", "seven", "eight", "nine",
+	}
+
+	trimmed := strings.Trim(string(file), "\n")
+	lines := strings.Split(trimmed, "\n")
+	sum := 0
+
+	for _, line := range lines {
+		var firstDigit, lastDigit int
+		firstPosition := math.MaxInt
+		lastPosition := -1
+
+		for i, b := range line {
+			isDigit := b >= '0' && b <= '9'
+			if !isDigit {
+				continue
+			}
+
+			digit := int(b - '0')
+			if i < firstPosition {
+				firstDigit = digit
+				firstPosition = i
+			}
+			lastDigit = digit
+			lastPosition = i
+		}
+
+		for digitIndex, digitText := range digits {
+			digit := digitIndex + 1
+
+			position := strings.Index(line, digitText)
+			if position != -1 {
+				if position < firstPosition {
+					firstDigit = digit
+					firstPosition = position
+				}
+			}
+
+			position = strings.LastIndex(line, digitText)
+			if position != -1 {
+				if position > lastPosition {
+					lastDigit = digit
+					lastPosition = position
+				}
+			}
+		}
+
+		sum += firstDigit*10 + lastDigit
+	}
+
+	fmt.Println(sum)
+}

0897b7f905d8046586cf2b603e636be86f90e8cb
Luna Razzaghipour <lunarazzaghipour@gmail.com>
2023-12-04 12:43:34 +1100

    Day 2 Part 1

---
+++ day2part1.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	file, err := os.ReadFile("day2input")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: failed to read input\n")
+		return
+	}
+
+	trimmed := strings.Trim(string(file), "\n")
+	games := strings.Split(trimmed, "\n")
+	idSum := 0
+
+	for i, game := range games {
+		if isGameValid(game) {
+			gameID := i + 1
+			idSum += gameID
+		}
+	}
+
+	fmt.Println(idSum)
+}
+
+func isGameValid(game string) bool {
+	_, sets, _ := strings.Cut(game, ": ")
+
+	for _, set := range strings.Split(sets, "; ") {
+		if !isSetValid(set) {
+			return false
+		}
+	}
+
+	return true
+
+}
+
+func isSetValid(set string) bool {
+	maxCountForColor := map[string]int{
+		"red":   12,
+		"green": 13,
+		"blue":  14,
+	}
+
+	for _, countAndColor := range strings.Split(set, ", ") {
+		countString, color, _ := strings.Cut(countAndColor, " ")
+		count, _ := strconv.Atoi(countString)
+
+		if count > maxCountForColor[color] {
+			return false
+		}
+	}
+
+	return true
+}

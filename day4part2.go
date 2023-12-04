98536b861dcda7c46a52e680b5d4d154af050985
Luna Razzaghipour <lunarazzaghipour@gmail.com>
2023-12-04 21:38:55 +1100

    Replace hashing with bitset in day 4 part 2

--- day4part2.go
+++ day4part2.go
@@ -2,13 +2,46 @@ package main
 
 import (
 	"fmt"
+	"math/bits"
 	"os"
+	"strconv"
 	"strings"
 )
 
+type numberSet struct {
+	lo, hi uint64
+}
+
+func (s *numberSet) insert(n int) {
+	if n > 63 {
+		s.hi |= 1 << (n - 64)
+	} else {
+		s.lo |= 1 << n
+	}
+}
+
+func (s *numberSet) insertSpaceSeparated(str string) {
+	for _, ns := range strings.Split(str, " ") {
+		if len(ns) == 0 {
+			continue
+		}
+
+		n, _ := strconv.Atoi(ns)
+		s.insert(n)
+	}
+}
+
+func (s1 numberSet) intersection(s2 numberSet) numberSet {
+	return numberSet{lo: s1.lo & s2.lo, hi: s1.hi & s2.hi}
+}
+
+func (s numberSet) count() int {
+	return bits.OnesCount64(s.lo) + bits.OnesCount64(s.hi)
+}
+
 type card struct {
-	winningNumbers map[string]bool
-	myNumbers      []string
+	winningNumbers numberSet
+	myNumbers      numberSet
 }
 
 func main() {
@@ -20,28 +53,17 @@ func main() {
 
 	trimmed := strings.Trim(string(file), "\n")
 	lines := strings.Split(trimmed, "\n")
-	cards := make([]card, 0)
+	cards := make([]card, len(lines))
 
-	for _, cardString := range lines {
-		_, cardString, _ := strings.Cut(cardString, ": ")
-		winningNumbersString, myNumbersString, _ :=
-			strings.Cut(cardString, " | ")
+	for i, card := range lines {
+		_, card, _ := strings.Cut(card, ": ")
+		winningNumbers, myNumbers, _ := strings.Cut(card, " | ")
 
-		card := card{winningNumbers: make(map[string]bool)}
-
-		for _, winningNumber := range strings.Split(winningNumbersString, " ") {
-			if len(winningNumber) == 0 {
-				continue
-			}
-
-			card.winningNumbers[winningNumber] = true
-		}
-
-		card.myNumbers = strings.Split(myNumbersString, " ")
-		cards = append(cards, card)
+		cards[i].winningNumbers.insertSpaceSeparated(winningNumbers)
+		cards[i].myNumbers.insertSpaceSeparated(myNumbers)
 	}
 
-	cardsToProcess := make([]int, 0)
+	cardsToProcess := make([]int, 0, len(cards))
 	for i := range cards {
 		cardsToProcess = append(cardsToProcess, i)
 	}
@@ -54,12 +76,7 @@ func main() {
 		card := cards[cardID]
 		cardsToProcess = cardsToProcess[:len(cardsToProcess)-1]
 
-		matchCount := 0
-		for _, myNumber := range card.myNumbers {
-			if card.winningNumbers[myNumber] {
-				matchCount++
-			}
-		}
+		matchCount := card.winningNumbers.intersection(card.myNumbers).count()
 
 		if matchCount == 0 {
 			continue
